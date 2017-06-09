1d827c1d2cbe0fcaf1a64a2b7471990d74e01f72
Joey Langley <jlangley@gohcl.com>
2017-06-09 12:32:21 -0400

    hooked up bottom nav and added scrollspy and smoothscrolling effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,7 @@ var $myGroup = $('#capabilitiesGroup');
 $myGroup.on('click','.toggleButton', function() {
     $myGroup.find('.collapse.show').collapse('toggle');
 });
+
+$('a').smoothScroll();
+
+$('body').scrollspy({ target: '#mainNav' })
